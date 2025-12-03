de7b771996c2b05fd0bc1373b843459b0ffe6f9d
sethflower <139699611+sethflower@users.noreply.github.com>
2025-12-03 11:56:46 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -43,7 +43,13 @@ if not all([BOT_TOKEN, SUPERADMIN_ID, DATABASE_URL]):
 
 logging.basicConfig(level=logging.INFO)
 
-bot = Bot(token=BOT_TOKEN, parse_mode=ParseMode.HTML)
+from aiogram.client.default import DefaultBotProperties
+
+bot = Bot(
+    token=BOT_TOKEN,
+    default=DefaultBotProperties(parse_mode=ParseMode.HTML)
+)
+
 dp = Dispatcher()
 
 ###############################################################
